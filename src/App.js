ec68c28b96a60eda0833ee709b8c001a029f8937
jagdish <jagdish0000singh@gmail.com>
2023-08-27 23:33:07 +0530

    hideshow text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// hide show text clicking button
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+   const[status,setStatus]=React.useState(true)
+    return(
+
+        <>
+        {
+           status?
+           <h1>hello jagdish</h1>
+           :null
+        }
+
+        <button onClick={()=>setStatus(false)}>hide </button>
+        <button onClick={()=>setStatus(true)}>show</button>
+        </>
+    )
+
+
 }
 
 export default App;
